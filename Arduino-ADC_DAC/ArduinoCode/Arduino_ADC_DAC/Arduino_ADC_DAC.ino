1a5acc6e9246ab94aa838a894b9753fae8be0970
Kevin Liang <kevinjliang2011@gmail.com>
2014-08-30 17:42:06 -0400

    **Untested Code** - Set up a basic test to see if we can get the LCD screen + Arduino working, but haven't set them up on the breadboard, so not sure if this code'll work yet

--- Arduino-ADC_DAC/ArduinoCode/Arduino_ADC_DAC/Arduino_ADC_DAC.ino
+++ Arduino-ADC_DAC/ArduinoCode/Arduino_ADC_DAC/Arduino_ADC_DAC.ino
@@ -1,1 +1,25 @@
+#include <Wire.h>
+#include <Adafruit_MCP23017.h>
+#include <Adafruit_RGBLCDShield.h>
 
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+
+void setup(){
+  // LCD initialization
+  lcd.begin(16, 2);
+  lcd.setBacklight(0x7);
+  
+  // Start up message
+  lcd.setCursor(0, 0);
+  lcd.print("Hey-");
+  lcd.setCursor(0, 1);
+  lcd.print("O");
+  delay(1000);
+  lcd.setCursor(0, 0);
+  lcd.clear();
+}
+
+
+void loop(){
+  
+}
