576f3397a9583e356bf2eb4bb92e1701dca3c786
Kevin Liang <kevinjliang2011@gmail.com>
2014-08-31 12:23:48 -0400

    Coded functionality to read input voltage at selected interval, smooth with sliding window averager, and display a value-corrected (account for analogRead and shift+gain circuit) voltage in a basic manner (just a single value). Compiles and runs "correctly." However, Arduino isn't reading the right values: seeing AC instead of DC.

--- Arduino-ADC_DAC/ArduinoCode/Arduino_ADC_DAC/Arduino_ADC_DAC.ino
+++ Arduino-ADC_DAC/ArduinoCode/Arduino_ADC_DAC/Arduino_ADC_DAC.ino
@@ -4,7 +4,32 @@
 
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
+
+//// Declare all variables here as global to avoid re-declaring the same variables in the loop() and running out of memory 
+//Timekeeping variables
+long currentTime = 0; //tracks how long it's been since the program began running
+long lastReadTime = 0; //the last time a reading was taken from the circuit
+
+//Input voltage variables
+float readVal; //voltage reading from pin A0, from circuit
+float trueVal; //readjusted true value
+  
+//Sliding Window Averager variables
+float slidingWindow[5] = {0,0,0,0,0}; //queue for the five most recent readings
+int nextElement = 0; //rotating index of the array to place the next read val
+float avg = 0; //sum of all elements in the queue
+int avgDenom = 0; //denominator to calculate average
+  
+//Zero-crossings detector variables
+boolean wasPositive = false; //was previous value positive; compare to look for a zero-crossing
+float hystThresh = 0.1; //Hysteresis threshold; must cross before another zero-crossing can be detected
+float lastZCrossTime = 0; //to track times between zero-crossings
+float frequency = 0; //equal to 1/(2*period between zero-crossings)  
+
+
 void setup(){
+  Serial.begin(9600);
+  
   // LCD initialization
   lcd.begin(16, 2);
   lcd.setBacklight(0x7);
@@ -18,29 +43,50 @@ void setup(){
   lcd.setCursor(0, 0);
   lcd.clear();
   
-  //Timekeeping variables
-  long currentTime = 0; //tracks how long it's been since the program began running
-  long lastReadTime = 0; //the last time a reading was taken from the circuit
-
-  //Input voltage variables
-  float readVal; //voltage reading from pin A0, from circuit
-  float trueVal; //readjusted true value
-  
-  //Sliding Window Averager variables
-  float slidingWindow[10] = {0,0,0,0,0,0,0,0,0,0}; //queue for the ten most recent readings
-  int nextElement = 0; //rotating index of the array to place the next read val
-  float avg = 0; //sum of all elements in the queue
-  int avgDenom = 0; //denominator to calculate average
-  
-  //Zero-crossings detector variables
-  boolean wasPositive = false;
-  float hystThresh = 0.1;
-  float lastZCrossTime = 0;
-  
-
 }
 
 
 void loop(){
+  currentTime = millis();
   
+  if(currentTime > lastReadTime + 200){ //run if 200 ms has elapsed since last execute
+    lastReadTime = currentTime; 
+    displayToLCD(read_signal());
+  }  
 }
+
+
+
+float read_signal(){
+  readVal = analogRead(1)* (5.0 / 1023.0); //analogRead maps 0-5 V to 0-1023
+  Serial.println(readVal);
+  //Undo the shift and gain circuit
+  trueVal = readVal*2 - 5;
+    
+  slidingWindow[nextElement] = trueVal;
+  avg = 0;
+  for(int i=0; i<5; i++){
+     avg = avg + slidingWindow[i];
+  }
+    
+  //rotate through the five elements of the queue
+  if(nextElement<4){
+    nextElement++;
+  }    
+  else{
+    nextElement = 0;  
+  }  
+    
+  //adjust denominator of average to the number of elements in queue
+  if(avgDenom<5){
+    avgDenom++;
+  }  
+     
+  return avg/avgDenom;
+}  
+
+void displayToLCD(float outVolt) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Voltage: "); lcd.print(outVolt);
+}  
